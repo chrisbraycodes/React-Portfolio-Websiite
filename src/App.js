52e1c49622211453e38aa57209f31b6c36f13303
Christopher Bray <comp.chris@icloud.com>
2024-12-04 20:36:48 -0800

    Add Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import { About, Projects, Contact } from './components/Sections';
 import Skills from './components/Skills';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
     const [isDarkMode, setIsDarkMode] = useState(() => {
